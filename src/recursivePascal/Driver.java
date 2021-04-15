b6cfb95a35dd910ff119dd695662a09543e2496d
dfilipski <danny.filipski@gmail.com>
2021-04-15 09:59:29 -0400

    Make a recursive version and prompt user

--- src/recursivePascal/Driver.java
+++ src/recursivePascal/Driver.java
@@ -1,8 +1,16 @@
 package recursivePascal;
 
+import java.util.Scanner;
+
 public class Driver {
     public static void main(String[] args) {
-        Pascal triangle = new Pascal(5);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.printf("How many rows of this glorious triangle would you like to generate? ");
+        int rows = sc.nextInt();
+        System.out.printf("\nGot it! %d rows coming up!\n\n", rows);
+
+        Pascal triangle = new Pascal(rows);
         System.out.println(triangle);
     }
 }
--- src/recursivePascal/Pascal.java
+++ src/recursivePascal/Pascal.java
@@ -16,7 +16,24 @@ public class Pascal {
             triangle[i][0] = 1;
             triangle[i][triangle[i].length - 1] = 1;
         }
-        fill();
+        //fill();
+        recursiveFill();
+    }
+
+    public void recursiveFill() {
+        for (int row = 1; row < triangle.length; ++row) {
+            for (int col = 1; col < triangle[row].length - 1; ++col) {
+                if (triangle[row][col] == 0)
+                    triangle[row][col] = triangle[row - 1][col - 1] + triangle[row - 1][col];
+            }
+        }
+    }
+
+    public int recursiveGet(int r, int c) {
+        if (triangle[r][c] != 0)
+            return triangle[r][c];
+        else
+            return recursiveGet(r - 1, c) + recursiveGet(r - 1, c - 1);
     }
 
     public void fill() {
