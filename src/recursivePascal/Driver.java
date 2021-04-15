79a8d1db92543f06490b15c0d453620a3e039a76
dfilipski <danny.filipski@gmail.com>
2021-04-15 09:35:31 -0400

    Make a non recursive version of the triangle

---
+++ src/recursivePascal/Driver.java
@@ -0,0 +1,8 @@
+package recursivePascal;
+
+public class Driver {
+    public static void main(String[] args) {
+        Pascal triangle = new Pascal(5);
+        System.out.println(triangle);
+    }
+}
--- src/recursivePascal/Pascal.java
+++ src/recursivePascal/Pascal.java
@@ -1,4 +1,41 @@
 package recursivePascal;
 
+import java.util.Arrays;
+
 public class Pascal {
+    int[][] triangle;
+
+    public Pascal(int rows) {
+        triangle = new int[rows][];
+
+        for (int i = 0; i < rows; ++i) {
+            triangle[i] = new int[i+1];
+        }
+
+        for (int i = 0; i < rows; ++i) {
+            triangle[i][0] = 1;
+            triangle[i][triangle[i].length - 1] = 1;
+        }
+        fill();
+    }
+
+    public void fill() {
+        for (int row = 1; row < triangle.length; ++row) {
+            for (int col = 1; col < triangle[row].length - 1; ++col) {
+                if (triangle[row][col] == 0)
+                    triangle[row][col] = triangle[row - 1][col - 1] + triangle[row - 1][col];
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        String out = "";
+
+        for (int[] row : triangle) {
+            System.out.println(Arrays.toString(row));
+        }
+
+        return out;
+    }
 }
