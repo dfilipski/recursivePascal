80be02aa6c06ff8eee755debfb2f21c4f2a6c0c4
dfilipski <danny.filipski@gmail.com>
2021-04-15 10:11:50 -0400

    Add humor

--- src/recursivePascal/Driver.java
+++ src/recursivePascal/Driver.java
@@ -8,6 +8,11 @@ public class Driver {
 
         System.out.printf("How many rows of this glorious triangle would you like to generate? ");
         int rows = sc.nextInt();
+        while (rows > 35 || rows < 0) {
+            System.out.printf("Not gonna work, bub. Pick a number less than 36 and greater than 0. ");
+            rows = sc.nextInt();
+        }
+
         System.out.printf("\nGot it! %d rows coming up!\n\n", rows);
 
         Pascal triangle = new Pascal(rows);
